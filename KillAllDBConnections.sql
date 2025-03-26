b1d443ab9a9347b5ed2b99f99d1191e91de8b8fd
Andre Valente <70276822+andrevbl@users.noreply.github.com>
2025-03-26 17:36:22 -0300

    Create KillAllDBConnections.sql

---
+++ KillAllDBConnections.sql
@@ -0,0 +1,13 @@
+Use Master
+Go
+Declare @dbname sysname
+Set @dbname = 'DataBaseName'
+Declare @spid int
+Select @spid = min(spid) from master.dbo.sysprocesses
+where dbid = db_id(@dbname)
+While @spid Is Not Null
+Begin
+        Execute ('Kill ' + @spid)
+        Select @spid = min(spid) from master.dbo.sysprocesses
+        where dbid = db_id(@dbname) and spid > @spid
+End
